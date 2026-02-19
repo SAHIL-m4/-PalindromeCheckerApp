66aa06f998b0d1f5d23e30d4603febd1df68dbef
jaslyn-j <jaslynjacob026@gmail.com>
2026-02-19 10:51:44 +0530

    UC1

--- src/PalindromeCheckerApp/UseCasePalindromeCheckerApp.java
+++ src/PalindromeCheckerApp/UseCasePalindromeCheckerApp.java
@@ -3,7 +3,26 @@ package PalindromeCheckerApp;
 import java.util.Scanner;
 public class UseCasePalindromeCheckerApp {
      public static void main(String[] args){
-       System.out.println("Welcome palindrome");
+         Scanner scanner = new Scanner(System.in);
+         System.out.println("Enter your string : ");
+         String input = scanner.nextLine();
+         boolean isPalindrome=true;
+         String correct = input.toLowerCase();
+         int length = correct.length();
+         for(int i=0;i<input.length()/2;i++) {
+             if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                 isPalindrome = false;
+                 break;
+             }
+         }
+         System.out.println("------");
+         System.out.println("PALINDROME CHECKING...............");
+         if(isPalindrome){
+             System.out.println("-> is palindrome");
+         }else{
+             System.out.println("-> NOT palindrome");
+         }
+         scanner.close();
     }
 }
 
