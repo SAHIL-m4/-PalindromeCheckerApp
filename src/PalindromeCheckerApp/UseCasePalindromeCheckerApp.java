88675f35580b245336be257bfda21b5a9850a72b
SAHIL-m4 <mohammedsahilmac4@gmail.com>
2026-03-04 16:57:32 +0530

    Add files via upload

--- src/PalindromeCheckerApp/UseCasePalindromeCheckerApp.java
+++ src/PalindromeCheckerApp/UseCasePalindromeCheckerApp.java
@@ -1,12 +1,34 @@
-package PalindromeCheckerApp;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
-import java.util.Scanner;
 public class UseCasePalindromeCheckerApp {
-     public static void main(String[] args){
-       System.out.println("Welcome palindrome");
+
+    public static void main(String[] args) {
+
+        String input = "civic";
+
+        Queue<Character> queue = new LinkedList<>();
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : input.toCharArray()) {
+            queue.add(c);
+            stack.push(c);
+        }
+
+        boolean isPalindrome = true;
+
+        while (!queue.isEmpty()) {
+            if (!queue.remove().equals(stack.pop())) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println(input + " is a Palindrome.");
+        } else {
+            System.out.println(input + " is NOT a Palindrome.");
+        }
     }
 }
-
-
-
-
