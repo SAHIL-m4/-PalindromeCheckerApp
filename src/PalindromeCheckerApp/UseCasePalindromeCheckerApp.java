7eba176a917935c5bee4f418f6b39d2daa5a3c01
SAHIL-m4 <mohammedsahilmac4@gmail.com>
2026-03-06 00:57:38 +0530

    Add files via upload

--- src/PalindromeCheckerApp/UseCasePalindromeCheckerApp.java
+++ src/PalindromeCheckerApp/UseCasePalindromeCheckerApp.java
@@ -1,12 +1,31 @@
-package PalindromeCheckerApp;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
-import java.util.Scanner;
 public class UseCasePalindromeCheckerApp {
-     public static void main(String[] args){
-       System.out.println("Welcome palindrome");
+
+    public static void main(String[] args) {
+
+        String input = "refer";
+
+        Deque<Character> deque = new ArrayDeque<>();
+
+        for (char c : input.toCharArray()) {
+            deque.addLast(c);
+        }
+
+        boolean isPalindrome = true;
+
+        while (deque.size() > 1) {
+            char first = deque.removeFirst();
+            char last = deque.removeLast();
+
+            if (first != last) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
     }
 }
-
-
-
-
