df509a0d9dffb491bafb04a8565c95a016c93434
SAHIL-m4 <mohammedsahilmac4@gmail.com>
2026-03-06 01:08:30 +0530

    Add files via upload

---
+++ src/PalindromeCheckerApp/UseCase9PalindromeCheckerApp.java
@@ -0,0 +1,23 @@
+public class UseCase9PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+        String input = "madam";
+        System.out.println("Input : " + input);
+
+        boolean isPalindrome = check(input, 0, input.length() - 1);
+        
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
+
+    private static boolean check(String s, int start, int end) {
+        if (start >= end) {
+            return true;
+        }
+
+        if (s.charAt(start) == s.charAt(end)) {
+            return check(s, start + 1, end - 1);
+        }
+
+        return false;
+    }
+}
