6ede511329f9a56df7cf3aa4d01442c199b9c967
Hastik172 <Hastik2003@gmail.com>
2026-03-06 10:29:21 +0530

    UC11: helolo use case eleven

--- src/PalindromeCheckerApp/UseCasePalindromeCheckerApp.java
+++ src/PalindromeCheckerApp/UseCasePalindromeCheckerApp.java
@@ -2,30 +2,36 @@ package PalindromeCheckerApp;
 
 import java.util.Scanner;
 public class UseCasePalindromeCheckerApp {
-     public static void main(String[] args){
-         Scanner scanner = new Scanner(System.in);
-         System.out.println("Enter your string : ");
-         String input = scanner.nextLine();
-         boolean isPalindrome=true;
-         String correct = input.toLowerCase();
-         int length = correct.length();
-         for(int i=0;i<input.length()/2;i++) {
-             if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
-                 isPalindrome = false;
-                 break;
-             }
-         }
-         System.out.println("------");
-         System.out.println("PALINDROME CHECKING...............");
-         if(isPalindrome){
-             System.out.println("-> is palindrome");
-         }else{
-             System.out.println("-> NOT palindrome");
-         }
-         scanner.close();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input: ");
+        String input = scanner.nextLine();
+        PalindromeService service = new PalindromeService();
+        boolean result = service.checkPalindrome(input);
+
+        System.out.println("Is Palindrome?: " + result);
+
+        scanner.close();
     }
 }
 
+class PalindromeService {
+
+    public boolean checkPalindrome(String input) {
+
+        int start = 0;
+        int end = input.length() - 1;
+        while (start < end) {
+
+            if (input.charAt(start) != input.charAt(end)) {
+                return false;
+            }
+
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
 
 
 
